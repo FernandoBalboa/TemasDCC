7f016b867e0242a24b5990d2b44924ddd1f71fd7
Rodrigo <rdelgadovega@gmail.com>
2017-06-16 16:44:48 +0000

    Add admin test for merge topics

--- temasapp/admin.py
+++ temasapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from myapp.models import Topics, Teacher
 
-# Register your models here.
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    ordering = ['name']
+    actions = [merge_topics]
+    
+    def merge_topics(self, request, query_set):
+        topic = query_set[0]
+        for obj in query_set[1:]:
+            teachers = Teacher.objects.filter(topics=obj.pk)
+            for t in teachers:
+                t.topics.add(topic)
+            obj.delete()
+    merge_topics.short_description = "Merge selected topics"
+
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Teacher)
--- temasapp/models.py
+++ temasapp/models.py
@@ -4,15 +4,14 @@ from django.db import models
 
 
 class Topic(models.Model):
-    name = models.TextField()
+    name = models.CharField(max_length=50, primary_key=True)
 
-    def __str__():
+    def __str__(self):
         return self.name
-
 
 class Teacher(models.Model):
     name = models.TextField()
     topics = models.ManyToManyField(Topic)
 
-    def __str__():
+    def __str__(self):
         return self.name
