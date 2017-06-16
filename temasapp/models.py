2f17a4cc0183b5998957e41233a36dab80764a77
Elisa Kauffmann <elisa.kauffmann.f@gmail.com>
2017-06-16 17:04:46 +0000

    show teachers and their topics

--- temasapp/models.py
+++ temasapp/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Topic(models.Model):
     name = models.TextField()
 
-    def __str__():
+    def __str__(self):
         return self.name
 
 
@@ -14,5 +14,5 @@ class Teacher(models.Model):
     name = models.TextField()
     topics = models.ManyToManyField(Topic)
 
-    def __str__():
+    def __str__(self):
         return self.name
