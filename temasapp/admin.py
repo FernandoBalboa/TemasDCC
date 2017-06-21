7de14184347ea49c06952bd6e40487cb90ee8900
Rodrigo <rdelgadovega@gmail.com>
2017-06-21 17:39:14 +0000

    fix admin.py teacher creation issue

--- temasapp/admin.py
+++ temasapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from models import Topic, Teacher
 
+"""
 class TopicAdmin(admin.ModelAdmin):
     list_display = ['name']
     ordering = ['name']
@@ -8,12 +9,13 @@ class TopicAdmin(admin.ModelAdmin):
     
     def merge_topics(self, request, query_set):
         topic = query_set[0]
+        print topic
         for obj in query_set[1:]:
             teachers = Teacher.objects.filter(topics=obj.pk)
             for t in teachers:
                 t.topics.add(topic)
             obj.delete()
     merge_topics.short_description = "Merge selected topics"
-
-admin.site.register(Topic, TopicAdmin)
+"""
+admin.site.register(Topic)
 admin.site.register(Teacher)
