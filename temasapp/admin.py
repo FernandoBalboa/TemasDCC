ac78d672ec0c6234249d33728368a16cdab2bb9f
Sebastián González M <seba-5@hotmail.com>
2017-06-23 12:31:21 -0400

    Add teacher data, closes #8

--- temasapp/admin.py
+++ temasapp/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from models import Topic, Teacher
 
+
 class TopicAdmin(admin.ModelAdmin):
     list_display = ['name']
     ordering = ['name']
     actions = ['merge_topics']
-    
+
     def merge_topics(self, request, query_set):
         topic = query_set[0]
         for obj in query_set[1:]:
